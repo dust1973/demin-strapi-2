e2a7cc34bd549a9a760be43aa6c5c4422f1705c4
Alexander Fuchs <alex-fuchs@online.de>
2022-03-13 16:22:40 +0100

    change db env

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_HOST', 'ec2-34-242-89-204.eu-west-1.compute.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      database: env('DATABASE_NAME', 'd100egqn9518gb'),
+      user: env('DATABASE_USERNAME', 'qbulwcawipcfyt'),
+      password: env('DATABASE_PASSWORD', '563679f94ef3b4a047c784a9f25c577fff1a1a9dda999c3005facb50dcce1f91'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
